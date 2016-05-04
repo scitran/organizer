cf8fae39fb9716bb66b83c764050c91be59eb2db
Renzo Frigato <rfrigato@stanford.edu>
2016-05-04 16:25:12 -0700

    list files in a system folder

--- renderer.js
+++ renderer.js
@@ -1,3 +1,29 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+var fs = require('fs');
+var exports = module.exports = {};
+
+exports.attachElement = function(path) {
+    var doc = document.getElementsByClassName('filelist')[0];
+    doc.textContent = '';
+    fs.exists(path, (exists) => {
+        if (exists) {
+            fs.readdir(path, (err, files) => {
+                if (err){
+                    doc.textContent = err;
+                }
+                else {
+                    files.forEach((f) => {
+                        var div = document.createElement('div');
+                        div.textContent = f;
+                        doc.appendChild(div);
+                    });
+                }
+            });
+        } else {
+            doc.textContent = 'path does not exist';
+        }
+    })
+    return false
+}
