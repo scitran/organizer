ad869a378428d52290b55f0cf3039412ffcdb271
Gunnar Schaefer <gsfr@stanford.edu>
2016-05-05 15:53:54 -0700

    Refactor recursion (#1)
    * Refactor recursion
    
    * Small fixes

--- renderer.js
+++ renderer.js
@@ -1,44 +1,46 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
-var fs = require('fs');
-var exports = module.exports = {};
 
-exports.attachElement = function(path) {
+var fs = require('fs');
+var exports = module.exports;
+
+module.exports.attachElement = function(path) {
     var doc = document.getElementsByClassName('filelist')[0];
     doc.textContent = '';
     fs.exists(path, (exists) => {
         if (exists) {
-            fs.readdir(path, (err, files) => {
-                if (err){
-                    doc.textContent = err;
-                }
-                else {
-                    files.forEach((f) => {
-                        recListDir(doc, f, path, '');
-                    });
+            fs.stat(path, (err, stat) => {
+                if (stat.isDirectory()) {
+                    dirList(doc, path, '');
+                } else {
+                    doc.textContent = 'path must be a directory';
                 }
             });
         } else {
             doc.textContent = 'path does not exist';
         }
-    })
-    return false
+    });
+    return false;
 }
 
-var recListDir = function(rootEl, path, path_so_far, indent) {
-    var div = document.createElement('div');
-    div.textContent = indent + path;
-    rootEl.appendChild(div);
-    var full_path = path_so_far + '/' + path;
-    console.log(full_path);
-    fs.stat(full_path, (err, stat) => {
-        if (stat.isDirectory()) {
-            fs.readdir(full_path, (err, files) => {
-                files.forEach((f) => {
-                    recListDir(div, f, full_path, '__' + indent);
+var dirList = function(root, dirPath, indent) {
+    console.log(dirPath);
+    fs.readdir(dirPath, (err, files) => {
+        files.forEach((f) => {
+            if (!f.startsWith('.')) {
+                var fPath = dirPath + '/' + f;
+                console.log(fPath);
+                fs.stat(fPath, (err, stat) => {
+                    var div = document.createElement('div');
+                    div.innerHTML = indent + f;
+                    if (stat.isDirectory()) {
+                        div.innerHTML += '/';
+                        dirList(div, fPath, '&nbsp;&nbsp;' + indent);
+                    }
+                    root.appendChild(div);
                 });
-            });
-        }
+            }
+        });
     });
 }
