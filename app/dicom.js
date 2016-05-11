ffa49f7472eec63bf409db429626b309b776251a
Renzo Frigato <rfrigato@stanford.edu>
2016-05-11 12:35:43 -0700

    sort dicoms in folder

--- app/dicom.js
+++ app/dicom.js
@@ -4,17 +4,28 @@ const fs = require('fs');
 const dicomParser = require('dicom-parser');
 const TAG_DICT = require('./dataDictionary.js').TAG_DICT;
 
+const REVERSE_TAG_DICT = new Map(
+  Object.keys(TAG_DICT).map(k => [TAG_DICT[k].name, TAG_DICT[k]])
+);
+
 const parse = (filePath) => {
   const dicomFileAsBuffer = fs.readFileSync(filePath);
   return dicomParser.parseDicom(dicomFileAsBuffer);
 };
 
 
-const getTag = (tag) => {
-  const group = tag.substring(1,5);
-  const element = tag.substring(5,9);
+const getTag = (key) => {
+  const group = key.substring(1,5);
+  const element = key.substring(5,9);
   const tagIndex = ('(' + group + ',' + element + ')').toUpperCase();
   return TAG_DICT[tagIndex];
+};
+
+const getKeyFromName = name => {
+  if (REVERSE_TAG_DICT.get(name) !== undefined){
+    const tag = REVERSE_TAG_DICT.get(name).tag;
+    return ('x' + tag.substring(1,5) + tag.substring(6,10)).toLowerCase();
+  }
 };
 
 const dicomDump = (filePath) => {
@@ -29,6 +40,29 @@ const dicomDump = (filePath) => {
   return dump;
 };
 
+const dicomSort = (files) => files.map(
+  (f) => {
+    let dp;
+    try {
+      dp = parse(f);
+    } catch (exc) {
+      return {name: f};
+    }
+    const session = dp.string(getKeyFromName('StudyInstanceUID'));
+    const acquisition = dp.string(getKeyFromName('SeriesInstanceUID'));
+    return {
+      name: f,
+      session: session,
+      acquisition: acquisition
+    };
+  }
+).filter(
+  (o) => {
+    return o.session !== undefined && o.acquisition !== undefined;
+  }
+);
+
 module.exports = {
-  dicomDump: dicomDump
+  dicomDump: dicomDump,
+  dicomSort: dicomSort
 };
--- app/renderer.js
+++ app/renderer.js
@@ -1,72 +1,115 @@
 'use strict';
 
 const fs = require('fs');
-const dicomDump = require('./dicom.js').dicomDump;
+const {dicomDump, dicomSort} = require('./dicom.js');
+const Rx = require('rxjs/Rx');
 
-var createDIVorDicomLink = function(fPath, f, indent) {
+const createAndAppendDIV = (parent, content) => {
+  let div = document.createElement('div');
+  div.innerHTML = content;
+  parent.appendChild(div);
+  return div;
+};
+
+const createAndAppendDicomLink = (parent, indent, content, fPath) => {
   let element = document.createElement('div');
   element.innerHTML = indent;
-  if (fPath.endsWith('.dcm')) {
-    let a = document.createElement('a');
-    a.onclick = function(){return window.renderer.appendDicomDump(fPath);};
-    a.setAttribute('href', '#');
-    a.innerHTML = f;
-    element.appendChild(a);
-  } else {
-    element.innerHTML += f;
-  }
-  return element;
+  let a = document.createElement('a');
+  a.onclick = function(){return window.renderer.appendDicomDump(fPath);};
+  a.setAttribute('href', '#');
+  a.innerHTML = content;
+  element.appendChild(a);
+  parent.appendChild(element);
 };
 
-const dirList = function(root, dirPath, indent) {
-  fs.readdir(dirPath, (err, files) => {
-    files.forEach((f) => {
-      if (!f.startsWith('.')) {
-        let fPath = dirPath + '/' + f;
-        fs.stat(fPath, (err, stat) => {
-          let element = createDIVorDicomLink(fPath, f, indent);
-          if (stat.isDirectory()) {
-            element.innerHTML += '/';
-            dirList(element, fPath, '&nbsp;&nbsp;&nbsp;' + indent);
-          }
-          root.appendChild(element);
-        });
-      }
-    });
-  });
-};
-
-const attachElement = function(path) {
-  let doc = document.getElementsByClassName('filelist')[0];
-  doc.textContent = '';
-  fs.exists(path, (exists) => {
-    if (exists) {
+const dirListObs = function(path) {
+  const _helperObs = path =>
+    Rx.Observable.create(function(observer){
       fs.stat(path, (err, stat) => {
         if (stat.isDirectory()) {
-          dirList(doc, path, '');
+          fs.readdir(path, (err, files) => {
+            files.forEach(f => observer.next(path + '/' + f));
+            observer.complete();
+          });
         } else {
-          doc.textContent = 'path must be a directory';
+          observer.complete();
         }
+
       });
-    } else {
-      doc.textContent = 'path does not exist';
+    });
+  return Rx.Observable
+    .of(path)
+    .expand(_helperObs);
+};
+
+class View {
+  constructor(root, indent) {
+    this.root = root;
+    this.sessions = new Map();
+    this.indent = indent;
+  }
+
+  appendDicom(dicom) {
+    if (!this.sessions.has(dicom.session)) {
+      this.sessions.set(
+        dicom.session,
+        {
+          acqMap: new Map(),
+          sesDiv: createAndAppendDIV(this.root, dicom.session)
+        }
+      );
     }
-  });
+    let {sesDiv, acqMap} = this.sessions.get(dicom.session);
+    if (!acqMap.has(dicom.acquisition)) {
+      acqMap.set(
+        dicom.acquisition,
+        createAndAppendDIV(sesDiv, this.indent + dicom.acquisition)
+      );
+    }
+    let acqDiv = acqMap.get(dicom.acquisition);
+    let fsplit = dicom.name.split('/');
+    let fname = fsplit[fsplit.length - 1];
+    createAndAppendDicomLink(
+      acqDiv, this.indent + this.indent, fname, dicom.name
+    );
+  }
+}
+
+const appendDicomSort = function(path) {
+  document.getElementsByClassName('dicom-dump')[0].innerHTML = '';
+  const doc = document.getElementsByClassName('dicom-sort')[0];
+  doc.innerHTML = '';
+  try {
+    fs.accessSync(path);
+  } catch (exc) {
+    createAndAppendDIV(doc, path + ' is not accessible on the filesystem.');
+  }
+  const view = new View(doc, '&nbsp;&nbsp;&nbsp;');
+  const obsFiles = dirListObs(path);
+  dicomSort(obsFiles).subscribe(
+    function(dicom) {
+      view.appendDicom(dicom);
+    },
+    function (err) {
+      console.log('Error: ' + err);
+    },
+    function () {
+      console.log('Completed');
+    }
+  );
   return false;
 };
 
 const appendDicomDump = function(path) {
   let doc = document.getElementsByClassName('dicom-dump')[0];
   doc.innerHTML = '';
-  dicomDump(path).forEach((line) => {
-    let div = document.createElement('div');
-    div.innerHTML = line;
-    doc.appendChild(div);
-  });
+  dicomDump(path).forEach(
+    (line) => createAndAppendDIV(doc, line)
+  );
   return false;
 };
 
 window.renderer = {
-  attachElement: attachElement,
-  appendDicomDump: appendDicomDump
+  appendDicomDump: appendDicomDump,
+  appendDicomSort: appendDicomSort
 };
