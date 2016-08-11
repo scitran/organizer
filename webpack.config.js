b938f2a16436a04f3ed39e95b14fd14197f39678
Renzo Frigato <rfrigato@stanford.edu>
2016-08-11 10:30:55 -0700

    add workaround to handle webpack issue
    https://github.com/webpack/webpack/issues/138

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,9 @@ module.exports = {
     ],
     loaders: [
       { test: /\.json$/, loader: 'json-loader' }
-    ]
+    ],
+    // workaround to solve https://github.com/webpack/webpack/issues/138
+    noParse: /node_modules\/json-schema\/lib\/validate\.js/
   },
   eslint: {
     failOnWarning: false,
