b1ffc4ddd757c57aad6af7a0df9af67b2654f3b4
Carlos Correa <carloscorrea137@gmail.com>
2016-10-18 13:30:41 -0700

    Add parameter to request library that throws for unsuccessful HTTP requests.

--- app/services/apiQueues.js
+++ app/services/apiQueues.js
@@ -2,6 +2,7 @@
 const angular = require('angular');
 const app = angular.module('app');
 const request = require('request');
+const url = require('url');
 
 app.factory('apiQueues', queues);
 queues.$inject = ['queueFactory'];
@@ -13,7 +14,16 @@ function queues(queueFactory) {
 
 function exec(message) {
   request(message.options, function(error, response, body){
-    if (error){
+    if (message.options.throwForStatus && response.statusCode >= 400) {
+      let msg = '';
+      try {
+        msg = JSON.parse(body).message + '\n';
+      } catch(e) {
+        console.error(`Could not parse response from server: ${body}`);
+      }
+      message._reject(new Error(`${msg}HTTP Server responded with ${response.statusCode} for
+${response.request.method} ${url.format(response.request.uri)}`));
+    } else if (error){
       console.log(error);
       message._reject(error);
     } else {
